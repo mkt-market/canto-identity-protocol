1c8eb78ad731faf816b5f4b4d0fd8b069189ef9e
Roman Böhringer <r.boehringer@opencore.ch>
2023-01-16 15:10:56 +0100

    Creation of subprotocol

--- src/SubprotocolRegistry.sol
+++ src/SubprotocolRegistry.sol
@@ -13,6 +13,22 @@ contract SubprotocolRegistry {
                                  STATE
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice Data that is associated with a subprotocol.
+    /// @dev Data types are chosen such that all data fits in one slot
+    struct SubprotocolData {
+        /// @notice Owner (registrant) of the subprotocol
+        address owner;
+        /// @notice Optional cost in NOTE to add an NFT
+        /// @dev Maximum value is (2^96 - 1) / 10^18 =~ 80 billion. Zero for no fee
+        uint96 fee;
+        address nftAddress;
+        bool ordered;
+        bool primary;
+        bool active;
+    }
+
+    /// @notice Mapping (name => data) that contains all registered subprotocols
+    mapping(string => SubprotocolData) public subprotocols;
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
@@ -22,7 +38,20 @@ contract SubprotocolRegistry {
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
+    error SubprotocolAlreadyExists(string name, address owner);
 
 
+    function register(bool ordered, bool primary, bool active, address nftAddress, string calldata name, uint96 fee) external {
+        SubprotocolData memory subprotocolData = subprotocols[name];
+        if (subprotocolData.owner != address(0))
+            revert SubprotocolAlreadyExists(name, subprotocolData.owner);
+        subprotocolData.owner = msg.sender;
+        subprotocolData.fee = fee;
+        subprotocolData.nftAddress = nftAddress;
+        subprotocolData.ordered = ordered;
+        subprotocolData.primary = primary;
+        subprotocolData.active = active;
+        subprotocols[name] = subprotocolData;
+    }
 
 }
