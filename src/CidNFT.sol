ec87450c413bea1c0dde26cda78036e20a351fe1
Roman Böhringer <r.boehringer@opencore.ch>
2023-01-16 20:41:04 +0100

    Started with addition logic

--- src/CidNFT.sol
+++ src/CidNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.0;
 
 import "solmate/tokens/ERC721.sol";
+import "SubprotocolRegistry.sol";
 
 /// @title Canto Identity Protocol NFT
 /// @notice CID NFTs are at the heart of the CID protocol. All key/values of subprotocols are associated with them.
@@ -20,37 +21,63 @@ contract CidNFT is ERC721 {
     /// @notice Wallet that receives CID fees
     address public immutable cidFeeWallet;
 
+    /// @notice NOTE contract address
+    address public immutable noteContract;
+
     /// @notice Base URI of the NFT
     string public baseURI;
+
+    /// @notice Reference to the subprotocol registry
+    SubprotocolRegistry immutable subprotocolRegistry;
 
     /*//////////////////////////////////////////////////////////////
                                  STATE
     //////////////////////////////////////////////////////////////*/
+
+    /// @notice The different types of value that can be added for a given key 
+    enum ValueType {
+        ORDERED,
+        PRIMARY,
+        ACTIVE
+    }
     
     /// @notice Counter of the minted NFTs
     /// @dev Used to assign a new unique ID. The first ID that is assigned is 1, ID 0 is never minted.
     uint public numMinted;
 
+    mapping (uint => mapping(uint => uint)) public CIDDataOrdered;
+
+    mapping (uint => mapping(string => uint)) public CIDDataPrimary;
+
+    mapping (uint => mapping(string => uint[])) public CIDDataActive;
+
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
 
-    error OnlyFactoryCanMint();
     error TokenNotMinted();
+    error SubprotocolDoesNotExist(string subprotocolName);
+    error InvalidValueTypeLengthCombination(ValueType valueType, uint nftIDsLength);
 
     /// @notice Sets the name, symbol, baseURI, and the address of the auction factory
     /// @param _name Name of the NFT
     /// @param _symbol Symbol of the NFT
     /// @param _baseURI NFT base URI. {id}.json is appended to this URI
     /// @param _cidFeeWallet Address of the wallet that receives the fees
+    /// @param _noteContract Address of the NOTE contract
+    /// @param _subprotocolRegistry Address of the subprotocol registry
     constructor(
         string memory _name,
         string memory _symbol,
         string memory _baseURI,
-        address _cidFeeWallet
+        address _cidFeeWallet,
+        address _noteContract,
+        address _subprotocolRegistry
     ) ERC721(_name, _symbol) {
         baseURI = _baseURI;
         cidFeeWallet = _cidFeeWallet;
+        noteContract = _noteContract;
+        subprotocolRegistry = SubprotocolRegistry(_subprotocolRegistry);
     }
 
     /// @notice Get the token URI for the provided ID
@@ -72,5 +99,17 @@ contract CidNFT is ERC721 {
         if (_addList.length != 0) {
             // TODO: call add with the provided calldata
         }
+    }
+
+    /// @param _key Key to set. This value is only relevant for the ValueType PRIMARY or ACTIVE (with strings as keys)
+    /// @param _keyID ID (integer key) to set. This value is only relevant for the ValueType ORDERED (with integers as keys)
+    /// @param _nftIDsToAdd The IDs of the NFTs to add. For type PRIMARY or ORDERED, one value must be provided. For type ACTIVE, multiple values can be provided
+    function add(uint _cidNftID, string _key, uint _keyID, string _subprotocolName, uint[] calldata _nftIDsToAdd, ValueType _type) external {
+        SubprotocolData memory subprotocolData = subprotocolRegistry.subprotocols(_subprotocolName);
+        if (subprotocolData.owner == address(0))
+            revert SubprotocolDoesNotExist(_subprotocolName);
+        if (((_type == ValueType.PRIMARY || _type == ValueType.ORDERED) && _nftIDsToAdd.length != 1) || 
+            (_type == ValueType.ACTIVE && _nftIDsToAdd.length == 0))
+            revert InvalidValueTypeLengthCombination(_type, _nftIDsToAdd.length);
     }
 }
