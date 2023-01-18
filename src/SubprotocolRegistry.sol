45df6aa128ceb29228725dc200822b8e9769e818
Roman Böhringer <r.boehringer@opencore.ch>
2023-01-18 16:50:52 +0100

    Events for SubprotocolRegistry

--- src/SubprotocolRegistry.sol
+++ src/SubprotocolRegistry.sol
@@ -2,12 +2,8 @@
 pragma solidity >=0.8.0;
 
 /// @title Subprotocol Registry
-/// @notice Allows to register new subprotocols
+/// @notice Enables registration of new subprotocols
 contract SubprotocolRegistry {
-    /*//////////////////////////////////////////////////////////////
-                                 ADDRESSES
-    //////////////////////////////////////////////////////////////*/
-
     /*//////////////////////////////////////////////////////////////
                                  STATE
     //////////////////////////////////////////////////////////////*/
@@ -32,11 +28,21 @@ contract SubprotocolRegistry {
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
+    event SubprotocolRegistered(
+        address indexed registrar,
+        string indexed name,
+        address indexed nftAddress,
+        bool ordered,
+        bool primary,
+        bool active,
+        uint96 fee
+    );
 
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error SubprotocolAlreadyExists(string name, address owner);
+    error NoTypeSpecified(string name);
 
     function register(
         bool _ordered,
@@ -46,15 +52,17 @@ contract SubprotocolRegistry {
         string calldata _name,
         uint96 _fee
     ) external {
+        if (!(_ordered || _primary || _active)) revert NoTypeSpecified(_name);
         SubprotocolData memory subprotocolData = subprotocols[_name];
         if (subprotocolData.owner != address(0)) revert SubprotocolAlreadyExists(_name, subprotocolData.owner);
         subprotocolData.owner = msg.sender;
         subprotocolData.fee = _fee;
-        subprotocolData.nftAddress = _nftAddress;
+        subprotocolData.nftAddress = _nftAddress; // TODO: Verify that is subprotocol NFT?
         subprotocolData.ordered = _ordered;
         subprotocolData.primary = _primary;
         subprotocolData.active = _active;
         subprotocols[_name] = subprotocolData;
+        emit SubprotocolRegistered(msg.sender, _name, _nftAddress, _ordered, _primary, _active, _fee);
     }
 
     function getSubprotocol(string calldata _name) external view returns (SubprotocolData memory) {
